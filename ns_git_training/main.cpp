95dc11ef3f11c26f0080b9ea319cf14c9148e387
Indika Wijayasinghe <indikabw@yahoo.com>
2023-08-07 14:03:56 -0400

    Initial Commit

---
+++ ns_git_training/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ns_git_training
+//
+//  Created by Indika Wijayasinghe on 8/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
